d0dadf2a61b04060cbc73a38d5aaa107c7ec5844
Jacqueline Quintanilla <jac.quintanilla@gmail.com>
2018-01-04 11:04:14 -0600

    subscription ran out

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './Landing';
 import Search from './Search';
+
+const FourOhFour = () => <h1>404</h1>;
 
 const App = () => (
 	<BrowserRouter>
 		<div className="app">
-			<Route exact path="/" component={Landing} />
-			<Route path="/search" component={Search} />
+			{/* switch makes a siwtch statement that only renders ONE component at a time and flips through them sequentially */}
+			<Switch>
+				<Route exact path="/" component={Landing} />
+				<Route path="/search" component={Search} />
+				<Route component={FourOhFour} />
+				{/* 404 goes last in component list. If nothing before matches, then you get a 404. */}
+			</Switch>
 		</div>
 	</BrowserRouter>
 );
